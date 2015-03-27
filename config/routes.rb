fb03b310a43592a47d0d7e582286618423ba299b
Morgan Riffer <technologyriff@gmail.com>
2015-03-27 15:27:56 -0400

    added devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'goals/index'
 
   get 'goals/show'
--- db/migrate/20150326121106_create_goals.rb
+++ db/migrate/20150326121106_create_goals.rb
@@ -2,11 +2,12 @@ class CreateGoals < ActiveRecord::Migration
   def change
     create_table :goals do |t|
       t.string :title
-      t.boolean :goal_met
+      t.boolean :goal_met, default: false
       t.text :body
       t.integer :priority, default: 0, null: false
 
       t.timestamps null: false
     end
+    add_foreign_key :users
   end
 end
